a90d8a4e4464dcb5f654374a9d7811b6bafeb8aa
Coolroo <coolrooswag@gmail.com>
2023-01-14 13:32:04 -0400

    updates

---
+++ backend/src/main/java/com/softdesign/plagueinc/PlagueincApplication.java
@@ -0,0 +1,13 @@
+package com.softdesign.plagueinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlagueincApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlagueincApplication.class, args);
+	}
+
+}
