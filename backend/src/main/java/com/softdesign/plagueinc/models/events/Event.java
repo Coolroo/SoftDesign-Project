f120df61eaa12f7e53e48836923b4c32d9a67234
Coolroo <coolrooswag@gmail.com>
2023-01-17 19:35:45 -0400

    Update Event.java

--- backend/src/main/java/com/softdesign/plagueinc/models/events/Event.java
+++ backend/src/main/java/com/softdesign/plagueinc/models/events/Event.java
@@ -2,7 +2,7 @@ package com.softdesign.plagueinc.models.events;
 
 import com.softdesign.plagueinc.models.plague.Plague;
 
-public abstract class Event {
+public interface Event {
 
     public void resolveEffect(Plague player){}
 
