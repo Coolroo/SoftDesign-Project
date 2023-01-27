21c598270719420b8fee3d0ae9959e134412d461
Coolroo <coolrooswag@gmail.com>
2023-01-26 20:21:26 -0400

    add websocket configuration

---
+++ backend/src/main/java/com/softdesign/plagueinc/configurations/WebSocketConfiguration.java
@@ -0,0 +1,21 @@
+package com.softdesign.plagueinc.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+    
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config){
+        config.enableSimpleBroker("/game");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry){
+        registry.addEndpoint("/websocket").withSockJS();
+    }
+}
