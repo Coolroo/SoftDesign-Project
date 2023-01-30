59a04dc37d3f68e148f8f8c4a37cbaf8dcf1d611
AMGirouxMtA <116688226+AMGirouxMtA@users.noreply.github.com>
2023-01-30 18:39:09 -0400

    trait card slots added to Plagues

--- frontend/src/components/PlagueCard.js
+++ frontend/src/components/PlagueCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PlagueCardTraitSlot from "./PlagueCardTraitSlot";
 
 class PlagueCard extends Component{
 
@@ -7,6 +8,11 @@ class PlagueCard extends Component{
             <React.Fragment>{
                     <div>
                         <img src={`/plaguecards/${this.props.cardName}.png`} alt="img"/>
+                        <div style={{top:'52.0%', left:'2.6%'}} className="plagueCardTraitSlot"><PlagueCardTraitSlot/></div>
+                        <div style={{top:'52.0%', left:'22.5%'}} className="plagueCardTraitSlot"><PlagueCardTraitSlot/></div>
+                        <div style={{top:'52.0%', left:'42.5%'}} className="plagueCardTraitSlot"><PlagueCardTraitSlot/></div>
+                        <div style={{top:'52.0%', left:'62.5%'}} className="plagueCardTraitSlot"><PlagueCardTraitSlot/></div>
+                        <div style={{top:'4.0%', left:'62.5%'}} className="plagueCardTraitSlot"><PlagueCardTraitSlot/></div>
                     </div>   
                 }
             </React.Fragment>
