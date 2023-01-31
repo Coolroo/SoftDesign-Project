bcbb7f687f13438e0feefdf6f1ad1ee03062dd2b
Wyatt Stagg <coolrooswag@gmail.com>
2023-01-31 15:50:34 -0400

    change city list to list

--- backend/src/main/java/com/softdesign/plagueinc/models/gamestate/selection_objects/CitySelection.java
+++ backend/src/main/java/com/softdesign/plagueinc/models/gamestate/selection_objects/CitySelection.java
@@ -2,6 +2,6 @@ package com.softdesign.plagueinc.models.gamestate.selection_objects;
 
 import com.softdesign.plagueinc.models.countries.Country;
 
-public record CitySelection(Country country, String city) {
+public record CitySelection(Country country, Integer city) {
     
 }
