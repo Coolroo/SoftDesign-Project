848e8082f3d4a0ee9c68e20d14dc77255a6bffbb
AMGirouxMtA <116688226+AMGirouxMtA@users.noreply.github.com>
2023-02-10 14:52:51 -0400

    Added Country Draft slot websocket interaction

--- frontend/src/components/CountryDraftZone.js
+++ frontend/src/components/CountryDraftZone.js
@@ -1,21 +1,22 @@
 import React, { Component } from "react";
-import BoardCountrySlot from "./BoardCountrySlot";
+import DraftCountrySlot from "./DraftCountrySlot";
 
 class PlagueCard extends Component{
 
     render() {
+        if(this.props.state.revealedCountries != null){
         return(
             <React.Fragment>{
                     <div className="countryDraftZone">
                         <img src={`/CountryDraftZone.png`} alt="img"/>
-                        <div style={{top:'2.4%', left:'29.4%'}} className="draftZoneCountrySlot"><BoardCountrySlot/></div>
-                        <div style={{top:'2.8%', left:'51.3%'}} className="draftZoneCountrySlot"><BoardCountrySlot/></div>
-                        <div style={{top:'3.2%', left:'74.7%'}} className="draftZoneCountrySlot"><BoardCountrySlot/></div>
+                        <div style={{top:'9.0%', left:'29.4%'}} className="draftZoneCountrySlot"><DraftCountrySlot name={this.props.state.revealedCountries[0]}/></div>
+                        <div style={{top:'9.4%', left:'51.3%'}} className="draftZoneCountrySlot"><DraftCountrySlot name={this.props.state.revealedCountries[1]}/></div>
+                        <div style={{top:'9.8%', left:'74.7%'}} className="draftZoneCountrySlot"><DraftCountrySlot name={this.props.state.revealedCountries[2]}/></div>
                     </div>   
                 }
             </React.Fragment>
         )
-    }
+    }}
 }
 
 export default PlagueCard;
