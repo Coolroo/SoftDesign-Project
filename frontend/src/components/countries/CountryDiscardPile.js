e36d9d80d2afe97a20745722a1aeaa0990dc4ea5
AMGirouxMtA <116688226+AMGirouxMtA@users.noreply.github.com>
2023-02-14 17:16:13 -0400

    Discard Pile functional

--- frontend/src/components/countries/CountryDiscardPile.js
+++ frontend/src/components/countries/CountryDiscardPile.js
@@ -3,15 +3,24 @@ import { Droppable } from 'react-drag-and-drop';
 
 class DraftDiscardPile extends Component{
     render() {
+        let pile = [];
+        if(this.props.state.game.countryDiscard.length > 0) {
+            pile.push (<img width='100%' src={"/countries/countrycardback.png"}/>)
+        }
+
         let discard = (data, event) => {
-            console.log("discard")
-            this.props.discard(data.country);
+            if (!this.props.state.game.readyToProceed){
+                console.log("discard")
+                this.props.discard(data.country);
+            }
         }    
     
         return(
-            <Droppable types={['country']} onDrop={discard}>
-                <div style={{top:'9.0%',left:'102.0%'}} className="countryDiscardPile"></div>
-            </Droppable>
+            <React.Fragment>
+                <Droppable types={['country']} onDrop={discard}>
+                    <div style={{top:'9.0%',left:'102.0%'}} className="countryDiscardPile">{pile}</div>
+                </Droppable>
+            </React.Fragment>
         )
     }
 }
